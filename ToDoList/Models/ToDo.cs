77e3a2d71041441e3abf6ede1eff223de45fe556
Elijah Morishita <elmorishita@dmacc.edu>
2023-07-14 14:10:01 -0500

    Added custom validation

--- ToDoList/Models/ToDo.cs
+++ ToDoList/Models/ToDo.cs
@@ -7,10 +7,12 @@ namespace ToDoList.Models
     {
         public int Id { get; set; }
 
+        // The "\s" allows spaces, use double "\\" to avoid using the escape character
         [Required(ErrorMessage = "Please enter a description.")]
+        [RegularExpression("^[a-zA-Z0-9\\s]+$", ErrorMessage ="Description may not contain any special characters.")]
         public string Description { get; set; }
 
-        [Required(ErrorMessage = "Please enter a due date.")]
+        [Today]
         public DateTime? DueDate { get; set; }
 
         [Required(ErrorMessage = "Please select a category.")]
---
+++ ToDoList/Models/TodayAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class TodayAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext ctx)
+        {
+            if(value is DateTime)
+            {
+                DateTime dateToCheck = (DateTime)value;
+                if(dateToCheck >= DateTime.Today)
+                {
+                    return ValidationResult.Success!;
+                }
+            }
+
+            // The ToString("d") causes DateTime to use the shorter version of the date (MM/DD/YYYY)
+            string msg = base.ErrorMessage ?? $"Please enter a {ctx.DisplayName} that's greater or equal to today (" + DateTime.Today.ToString("d") + ")";
+            return new ValidationResult(msg);
+        }
+    }
+}
